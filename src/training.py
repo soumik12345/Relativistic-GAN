21f76aa80ced79d93d8282d8ca8957565c7fe9fd
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-11-21 18:23:04 +0530

    Added Training Function

---
+++ src/training.py
@@ -0,0 +1,47 @@
+import torch, time
+from tqdm import tqdm
+from config import *
+from .utils import *
+
+
+def train(netG, netD, optimizerG, optimizerD, data_loader, device):
+    '''Training Function
+    Params:
+        netG        -> Generator Network
+        netD        -> Discriminator Network
+        optimizerG  -> Generator Optimizer
+        optimizerD  -> Discriminator Optimizer
+        data_loader -> Data Loader
+        device      -> Device
+    '''
+    g_loss_history, d_loss_history = [], []
+    for epoch in range(1, EPOCHS + 1):
+        print('Epoch', str(epoch), 'going on....')
+        start = time.time()
+        for index, real_images in tqdm(enumerate(data_loader), total=len(data_loader)):
+            netD.zero_grad()
+            real_images = real_images.to(device)
+            batch_size = real_images.size(0)
+            labels = torch.full((batch_size, 1), real_label, device=device)
+            outputR = netD(real_images)
+            noise = torch.randn(batch_size, nz, 1, 1, device=device)
+            fake = netG(noise)
+            outputF = netD(fake.detach())
+            errD = (torch.mean((outputR - torch.mean(outputF) - labels) ** 2) + 
+                    torch.mean((outputF - torch.mean(outputR) + labels) ** 2))/2
+            errD.backward(retain_graph=True)
+            optimizerD.step()
+            netG.zero_grad()
+            outputF = netD(fake)   
+            errG = (torch.mean((outputR - torch.mean(outputF) + labels) ** 2) +
+                    torch.mean((outputF - torch.mean(outputR) - labels) ** 2))/2
+            errG.backward()
+            optimizerG.step()
+            if (index + 1) % (len(data_loader) // 2) == 0:
+                g_loss_history.append(errG.item())
+                d_loss_history.append(errD.item())
+        print('Discriminator Loss:{}\tGenerator Loss:{}'.format(d_loss_history[-1], g_loss_history[-1]))
+        print('Time taken for epoch {} is {} sec\n'.format(epoch, time.time() - start))
+        if epoch % 10 == 0:
+            visualize_results(netG, device)
+    return g_loss_history, d_loss_history, netG
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,21 @@
-from torch import device
+from config import *
+from torch import device, randn
 from torch.cuda import is_available
 
 
 def get_device():
     '''Get Device'''
     return device("cuda" if is_available() else "cpu")
+
+
+def visualize_results(netG, device):
+    '''Visualize Generator Results
+    Params:
+        netG    -> Generator Network
+        device  -> Device
+    '''
+    noise = torch.randn(1, LATENT_DIMENSION, 1, 1, device=device)
+    gen_image = netG(noise).to("cpu").clone().detach().squeeze(0)
+    gen_image = gen_image.numpy().transpose(1, 2, 0)
+    plt.imshow((gen_image + 1) / 2)
+    plt.show()
